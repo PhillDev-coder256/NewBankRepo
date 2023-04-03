31a1653909b6847e7385894e41d19dfea505d832
PhillDev-coder256 <phillipphilcourts540@gmail.com>
2023-04-03 15:33:43 +0300

    git status

---
+++ taim/taim/taim_global.js
@@ -0,0 +1,39 @@
+const dash = () =>{
+    const image = document.querySelector('.prof-image');
+    const board = document.querySelector('.prof-icon');
+
+    image.addEventListener('click', (e)=>{
+        e.preventDefault()
+        board.classList.toggle('prof-icon-active')
+    })
+}
+
+dash();
+
+const showActive = () =>{
+  
+    const activePage = window.location.pathname;
+    const myLinks = document.querySelectorAll('.myLink');
+    const home = document.getElementById('homeLink');
+
+    myLinks.forEach(link => {
+      if (link.href.includes(activePage)) {
+        if (activePage.length === 1){
+            home.style.color = "#ffbb00";
+            console.log('mypage');
+        }
+        else{
+          link.style.color = '#ffbb00';
+        //   home.style.color = "#00ff00";
+          console.log(activePage)
+        }
+       
+      }
+      else{
+        link.style.color = "ffffff";
+      }
+    });
+  
+  }
+
+showActive();
